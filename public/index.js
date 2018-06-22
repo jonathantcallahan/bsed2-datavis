7026f033e3e273cf9b18d77478d2c782e42d8d31
Jonathan <jtc7537@gmail.com>
2018-06-22 17:29:14 -0400

    asdf

--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
 
 
-d3.selectAll('li').style('color', function(d, i){
-    return i % 2 ? '#fff' : '#eee';
-})
+d3.selectAll('li')
+    .data([2,5,6,12,123,5,33])
+        .style('font-size', d => { return `${d}px`});
+
+d3.select('#box').transition()
+    .style('background-color','black')
 
 
