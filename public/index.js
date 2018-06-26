5745d1dad4da6688af85a1f8f1586fa360a3d40d
Jonathan <jtc7537@gmail.com>
2018-06-26 17:07:34 -0400

    chart

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ d3.selectAll('li')
         .style('font-size', d => { return `${d}px`});
 
 d3.select('#box')
-    .on('mouseout', () => {
+    .on('mouseover', () => {
         console.log('mouseover')
         d3.select('#box')
             .transition()
@@ -13,7 +13,7 @@ d3.select('#box')
             .style('background-color','black')
             .style('height','4px')
     })
-    .on('mouseover', () => {
+    .on('mouseout', () => {
         d3.select('#box')
             .transition()
             .duration(500)
@@ -21,5 +21,14 @@ d3.select('#box')
             .style('height','100px')
     })
     
+const data = [12,44,23,55,23,53,12,12,44,52,62,88]
+
+d3.select('#chart')
+    .selectAll('div')
+    .data(data)
+        .enter()
+        .append('div')
+        .style('width', d => { return d + 'px'})
+        .text(d => { return d })
 
 
