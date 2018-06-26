45d5fe34db9c076c28da82a4b7f24a070b888972
Jonathan <jtc7537@gmail.com>
2018-06-26 16:27:05 -0400

    animations

--- public/index.js
+++ public/index.js
@@ -4,7 +4,22 @@ d3.selectAll('li')
     .data([2,5,6,12,123,5,33])
         .style('font-size', d => { return `${d}px`});
 
-d3.select('#box').transition()
-    .style('background-color','black')
+d3.select('#box')
+    .on('mouseout', () => {
+        console.log('mouseover')
+        d3.select('#box')
+            .transition()
+            .duration(500)
+            .style('background-color','black')
+            .style('height','4px')
+    })
+    .on('mouseover', () => {
+        d3.select('#box')
+            .transition()
+            .duration(500)
+            .style('background-color','white')
+            .style('height','100px')
+    })
+    
 
 
